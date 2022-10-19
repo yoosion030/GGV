852842c59784cad78076c2b507b12650638ae018
yoosion030 <rsy5487@naver.com>
2022-10-19 15:55:20 +0900

    Add export

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,6 @@
-export { default as Movie } from './Movie';
+// page
 export { default as MainPage } from './MainPage';
+export { default as DetailPage } from './DetailPage';
+//component
+export { default as Movie } from './Movie';
+export { default as MovieDetail } from './MovieDetail';

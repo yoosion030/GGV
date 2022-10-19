2eb393154fb5ff657abc8b4b734cd9af9cb872c7
yoosion030 <rsy5487@naver.com>
2022-10-19 15:55:40 +0900

    Add DetailType

---
+++ src/types/MovieDetail.ts
@@ -0,0 +1,13 @@
+export interface DetailData {
+  data: MovieDetailType;
+}
+
+export interface MovieDetailType {
+  id: number;
+  title: string;
+  poster_path: string;
+  release_date: string;
+  homepage: string;
+  overview: string;
+  
+}

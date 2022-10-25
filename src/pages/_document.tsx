62c05cbc9aa1b39955f0327378d17d7dfab275de
yoosion030 <rsy5487@naver.com>
2022-10-25 19:12:15 +0900

    Add style

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -15,12 +15,7 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head>
-          <link
-            href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@500;700;900&display=swap"
-            rel="stylesheet"
-          />
-        </Head>
+        <Head></Head>
         <body>
           <Main />
           <NextScript />

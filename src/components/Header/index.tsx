1b5e896ba69a79ed63ed1d33ab16175aabf1103f
yoosion030 <rsy5487@naver.com>
2022-10-26 10:51:57 +0900

    Add HeaderName

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,19 @@
 import * as S from './style';
 import * as I from 'assets/svg';
+import { useEffect, useState } from 'react';
 
 const Header = () => {
+  const [name, setName] = useState<string | null>('');
+  useEffect(() => {
+    setName(window.localStorage.getItem('name'));
+  }, []);
   return (
     <S.Header>
       <S.Nav>
         <I.Logo />
         <S.NavWrapper>
           <S.NavContent href="/">영화 리스트</S.NavContent>
-          <S.NavContent href="/user">김준님 반가워요!</S.NavContent>
+          <S.NavContent href="/user">{name}님 반가워요!</S.NavContent>
         </S.NavWrapper>
       </S.Nav>
     </S.Header>

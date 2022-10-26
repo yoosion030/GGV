d1c0f857ec3b9a3127541d5dd9cc4fbd43e8c971
yoosion030 <rsy5487@naver.com>
2022-10-26 11:17:21 +0900

    Add NotoSans

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -19,6 +19,10 @@ class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
+            href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@500;600;700;900&display=swap"
+            rel="stylesheet"
+          />
+          <link
             href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
             rel="stylesheet"
           />

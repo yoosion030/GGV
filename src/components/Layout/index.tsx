9dede63428e60fd8b22ab6367163033002a4ccbf
yoosion030 <rsy5487@naver.com>
2022-12-01 20:31:22 +0900

    Add ChildrenProps

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
+import { ChildrenPropsType } from 'types/Children';
 import * as S from './style';
 
-interface LayoutProps {
-  children: React.ReactNode;
-}
-const Layout: React.FC<LayoutProps> = ({ children }) => {
+const Layout: React.FC<ChildrenPropsType> = ({ children }) => {
   return <S.Layout>{children}</S.Layout>;
 };
 
---
+++ src/types/Children.ts
@@ -0,0 +1,3 @@
+export interface ChildrenPropsType {
+  children: React.ReactNode;
+}

e6b3c7c64a42071c35c8f95e73e8392ad4731b9a
yoosion030 <rsy5487@naver.com>
2023-02-01 12:36:30 +0900

    :wastebasket: 주석 삭제

--- src/atoms/AtomContainer.tsx
+++ src/atoms/AtomContainer.tsx
@@ -1,27 +1,8 @@
 import { atom } from 'jotai';
 import { SelectType } from 'types/Select';
 
-/**
- * 좋아요 누른 영화 아이디 배열
- */
-// export const LikeMovie = atom<number[] | null | undefined>({
-//   key: 'LikeMovie',
-//   default: null,
-// });
-
 export const LikeMovie = atom<number[] | null | undefined>([]);
-
-/**
- * 상영 예정 / 상영 중 영화 선택
- */
-// export const Select = atom<SelectType>({
-//   key: 'Select',
-//   default: 'now_playing',
-// });
 
 export const Select = atom<SelectType>('now_playing');
 
-/**
- * 필터 선택 Id
- */
 export const GenreId = atom<number | null>(null);

1cfc51d42d15edb80e9ce7f96a1bb119bbd0334b
yoosion030 <yoosioff@gmail.com>
2023-05-12 14:20:04 +0900

    Add QueryClientProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,19 @@
 import type { AppProps } from 'next/app';
 import { GlobalStyle } from 'shared/styles/GlobalStyle';
-
+import { QueryClient, QueryClientProvider } from 'react-query';
 import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const queryClient = new QueryClient();
+
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Head>
         <title>GGV</title>
       </Head>
       <GlobalStyle />
       <Component {...pageProps} />
-    </>
+    </QueryClientProvider>
   );
 }
 

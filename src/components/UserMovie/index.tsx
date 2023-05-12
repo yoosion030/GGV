818423afe11911860ab38c9273ca3951d8d1fae7
yoosion030 <yoosioff@gmail.com>
2023-05-12 14:59:27 +0900

    Add getUserMovieDetail useQuery

--- src/components/UserMovie/index.tsx
+++ src/components/UserMovie/index.tsx
@@ -1,35 +1,32 @@
 import axios from 'axios';
-import { useEffect, useState } from 'react';
-import { MovieDetailDataType, MovieDetailType } from 'types/MovieDetail';
+import { MovieDetailType } from 'types/MovieDetail';
 import { Movie } from 'components';
-
+import { useQuery } from 'react-query';
 interface UserMovieProps {
   id: number;
 }
 
 const UserMovie = ({ id }: UserMovieProps) => {
-  const [movie, setMovie] = useState<MovieDetailType>(); // prop으로 받아온 id의 영화
-
-  /**
-   * prop으로 넘어온 id의 영화 데이터 구하기
-   */
-  const getData = async () => {
-    try {
-      const { data }: MovieDetailDataType = await axios.get(
-        `${process.env.BASE_URL}/${id}?api_key=${process.env.API_KEY}`,
-      );
-      setMovie(data);
-    } catch (e) {
-      console.log(e);
-    }
+  const getUserMovieDetail = () => {
+    return axios.get<MovieDetailType>(
+      `${process.env.BASE_URL}/${id}?api_key=${process.env.API_KEY}`,
+    );
   };
 
-  useEffect(() => {
-    getData();
-  }, []);
+  const { data, isLoading } = useQuery(
+    `getUserMovieDetail-${id}`,
+    getUserMovieDetail,
+  );
 
-  // 받아온 영화 데이터를 Movie 컴포넌트에 전송
-  return <>{movie && <Movie movie={movie} />}</>;
+  return (
+    <>
+      {isLoading ? (
+        <h1>로딩중입니다.</h1>
+      ) : (
+        <div>{data && <Movie movie={data?.data} />}</div>
+      )}
+    </>
+  );
 };
 
 export default UserMovie;
